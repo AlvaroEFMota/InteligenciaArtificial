4bc4f61c98b98b79b0d56ef83792af0729d6fc38
Alvaro <alvaro11318@gmail.com>
2021-08-17 15:32:15 -0300

    Organizando os arquivos

--- download_web_page.py
+++
@@ -1,8 +0,0 @@
-import urllib.request, urllib.error, urllib.parse
-
-url = 'https://pme.estadao.com.br/blogs/blog-do-empreendedor/negocio-de-impacto-atua-na-identificacao-de-superdotacao-e-autismo/'
-
-response = urllib.request.urlopen(url)
-webContent = response.read()
-
-print(webContent[0:300])
